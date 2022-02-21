0b9263622225ee4965416ffa916cba18c8fc508d
Lucasci20 <lucascipriano2002@gmail.com>
2022-02-21 17:05:05 -0300

    version 1.6

--- src/components/StyledComponents/CardContainer.js
+++ src/components/StyledComponents/CardContainer.js
@@ -1,36 +1,37 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const CardContainer = styled.div`
-    width: 48%;
-    height: 370px;
+  width: 48%;
+  height: 370px;
 
-    padding-top: 13px;
-    padding-bottom: 30px;
-    padding-left: 14px;
-    padding-right: 19px;
+  padding-top: 13px;
+  padding-bottom: 30px;
+  padding-left: 14px;
+  padding-right: 19px;
 
-    background: #FFFFFF;
-    border: 0.5px solid #E4E4E4;
-    box-sizing: border-box;
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.05);
-    border-radius: 5px;
+  background: #ffffff;
+  border: 0.5px solid #e4e4e4;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
 
-    .chart {
+  .chart {
     height: 230px;
+    overflow-x: auto;
 
     .apexcharts-legend.apx-legend-position-right {
-        display: none;
+      display: none;
     }
-}
+  }
 
-    @media(max-width: 1320px) {
-        margin-top: 15px;
-    }
+  @media (max-width: 1320px) {
+    margin-top: 15px;
+  }
 
-    @media(max-width: 850px) {
-        width: 100%;
-    }
-    @media(max-width: 440px) {
-        height: 380px;
-    }
-`
+  @media (max-width: 850px) {
+    width: 100%;
+  }
+  @media (max-width: 440px) {
+    height: 380px;
+  }
+`;
