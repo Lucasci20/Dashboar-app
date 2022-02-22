f97d48f9d968162459e26c070777e56744f56949
Lucasci20 <lucascipriano2002@gmail.com>
2022-02-22 15:01:59 -0300

    version 1.9.0

--- src/components/Aside/ActionPlanChart/AsideActionPlans.js
+++ src/components/Aside/ActionPlanChart/AsideActionPlans.js
@@ -1,29 +1,29 @@
-import Title from '../../StyledComponents/Title'
-import Header from '../../StyledComponents/Header'
+import Title from "../../StyledComponents/Title";
+import Header from "../../StyledComponents/Header";
 
-import CircleChart from './CircleChart';
+import CircleChart from "./CircleChart";
 
-import './style.scss'
+import "./style.scss";
 import { FiSettings } from "react-icons/fi";
 
-import './style.scss'
+import "./style.scss";
 
 export default function AsideActionPlans() {
-    return (
-        <div className="aside-action-plans-container">
-            <Header>
-                <Title>
-                    Plano de ação
-                    <p>Status do dia</p>
-                </Title>
-                <i>
-                    <FiSettings />
-                </i>
-            </Header>
+  return (
+    <div className="aside-action-plans-chart">
+      <Header>
+        <Title>
+          Plano de ação
+          <p>Status do dia</p>
+        </Title>
+        <i>
+          <FiSettings />
+        </i>
+      </Header>
 
-            <div className='chart'>
-                <CircleChart />
-            </div>
-        </div>
-    )
+      <div className="chart">
+        <CircleChart />
+      </div>
+    </div>
+  );
 }
