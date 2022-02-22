563747f20c66fa6a46717a5700b449448432d062
Lucasci20 <lucascipriano2002@gmail.com>
2022-02-22 11:36:38 -0300

    version 1.8

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,41 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import { IoMenuOutline } from "react-icons/io5"
-import { MdInsertChartOutlined } from "react-icons/md"
-import { BsChevronLeft } from "react-icons/bs"
+import { IoMenuOutline } from "react-icons/io5";
+import { MdInsertChartOutlined } from "react-icons/md";
+import { BsChevronLeft } from "react-icons/bs";
 
-import './sidebar.scss'
+import "./sidebar.scss";
 
 export default function Sidebar() {
-    const [sidebar, setSidebar] = useState(false)
+  const [sidebar, setSidebar] = useState(false);
 
-    const ShowSidebar = () => setSidebar(!sidebar)
+  const ShowSidebar = () => setSidebar(!sidebar);
 
-    return (
-        <div className="sidebar">
-            <div className="sidebar-menu" >
-                <i className="menu-bars" onClick={ShowSidebar}>
-                    <IoMenuOutline/>
-                </i>
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar-menu">
+        <button className="menu-bars" onClick={ShowSidebar}>
+          <i>
+            <IoMenuOutline />
+          </i>
+        </button>
+      </div>
 
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className="nav-menu-items">
-                    <li>
-                        <a className="control-panel" href='/'>
-                            <i>
-                                <MdInsertChartOutlined />
-                            </i>
-                            <span>Painel de controle</span>
-                        </a>
-                    </li>
-                    <div className="navbar-toggle">
-                        <i onClick={ShowSidebar}>
-                           <BsChevronLeft />
-                        </i>
-                    </div>
-                </ul>
-            </nav>
-        </div>
-    )
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li>
+            <a className="control-panel" href="/">
+              <i>
+                <MdInsertChartOutlined />
+              </i>
+              <span>Painel de controle</span>
+            </a>
+          </li>
+          <div className="navbar-toggle">
+            <i onClick={ShowSidebar}>
+              <BsChevronLeft />
+            </i>
+          </div>
+        </ul>
+      </nav>
+    </div>
+  );
 }
